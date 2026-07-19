866584dd1a421ca9e29bd65566146eecf9338176
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-19 23:58:26 +0530

    worked on hero card

--- src/customer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import Button from "@mui/material/Button";
+
 
 const HomeSectionCarousel = () => {
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
 
-    const responsive = {
-    0: { item: 1 },
-    720: { item: 3 },
-    1024: { item: 4 },
-};
-
-const item = [1,1,1,1,1].map((item) => <HomeSectionCard/>)
+  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
   return (
-
-    <div>
-        <AliceCarousel
-        items={item}
+    <div className="relative px-4 lg:px-8">
+      <div className="relative px-5">
+      <AliceCarousel
+        items={items}
         disableButtonsControls
-        autoPlay
-        autoPlayInterval={3000}
         infinite
         responsive={responsive}
-    />
+      />
+      <Button variant="contained" className="z-50" sx={{position: 'absolute', top:"8rem" , right:"0rem" }}aria-label="next">
+        <ArrowCircleLeftIcon/>
+      </Button>
+      </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default HomeSectionCarousel
