912b12b31601ffc499446da7f9fd710b174e05ed
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-20 19:46:10 +0530

    feat: implement one section in homesection components with data

--- src/customer/Pages/HomePage/HomePage.jsx
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import MainCarousel from '../../component/HomeCarousel/MainCarousel'
 import HomeSectionCarousel from '../../component/HomeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/mens_kurta'
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
 
-        <div>
-            <HomeSectionCarousel/>
+        <div className='py-20 space-y-10 flex flex-col justify-center px-5 lg:px-10'>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Shoes"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Shirts"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Sarees"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Dress"}/>
+           
         </div>
     </div>
   )
