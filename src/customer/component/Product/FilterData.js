0c97d749feae431ed5f73cd20cfba3bc133a7b25
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-25 18:44:11 +0530

    fix: Solve Filter bar issue

--- src/customer/component/Product/FilterData.js
+++ src/customer/component/Product/FilterData.js
@@ -1,4 +1,4 @@
-const filters = [
+export const filters = [
   {
     id: 'color',
     name: 'Color',
@@ -49,6 +49,30 @@ export const singleFilter = [
       { value: '1999-2999', label: '₹1999 To ₹2999'},
       { value: '2999-4999', label: '₹2999 To ₹4999'},
       { value: '4999+', label: '₹4999+'},
+    ],
+  },
+
+  {
+    id: 'discount',
+    name: 'Discount Range',
+    options: [
+      { value: '10', label: '10% and above'},
+      { value: '20', label: '20% and above'},
+      { value: '30', label: '30% and above'},
+      { value: '40', label: '40% and above'},
+      { value: '50', label: '50% and above'},
+      { value: '60', label: '60% and above'},
+      { value: '70', label: '70% and above'},
+      { value: '80', label: '80% and above'},
+    ],
+  },
+
+  {
+    id: 'stock',
+    name: 'Availability',
+    options: [
+      { value: 'in_stock', label: 'Available'},
+      { value: 'out_of_stock', label: 'Out Of Stock'},
     ],
   },
 ];
--- src/customer/component/Product/Product.jsx
+++ src/customer/component/Product/Product.jsx
@@ -17,6 +17,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 import { mens_kurta } from '../../../Data/mens_kurta'
 import ProductCard from './ProductCard'
+import { filters, singleFilter } from './FilterData'
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
@@ -62,18 +63,14 @@ export default function Product() {
                 </button>
               </div>
 
-              {/* Filters */}
+              {/* Mobile Filters */}
+              <div className='flex flex-col'>
               <form className="mt-4 border-t border-gray-200">
-
                 {filters.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
                     <h3 className="-mx-2 -my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
-                        <span className="ml-6 flex items-center">
-                          <PlusIcon aria-hidden="true" className="size-5 group-data-open:hidden" />
-                          <MinusIcon aria-hidden="true" className="size-5 group-not-data-open:hidden" />
-                        </span>
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
@@ -124,6 +121,62 @@ export default function Product() {
                   </Disclosure>
                 ))}
               </form>
+                <form className="mt-4 border-t border-gray-200">
+                {singleFilter.map((section) => (
+                  <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
+                    <h3 className="-mx-2 -my-3 flow-root">
+                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                        <span className="font-medium text-gray-900">{section.name}</span>
+                       
+                      </DisclosureButton>
+                    </h3>
+                    <DisclosurePanel className="pt-6">
+                      <div className="space-y-4">
+                        {section.options.map((option, optionIdx) => (
+                          <div key={option.value} className="flex gap-3">
+                            <div className="flex h-5 shrink-0 items-center">
+                              <div className="group grid size-4 grid-cols-1">
+                               <input
+                                  defaultValue={option.value}
+                                  id={`filter-mobile-${section.id}-${optionIdx}`}
+                                  name={section.id}
+                                  type="radio"
+                                  className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                                />
+                                <svg
+                                  fill="none"
+                                  viewBox="0 0 14 14"
+                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                                >
+                                  <path
+                                    d="M3 8L6 11L11 3.5"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="opacity-0 group-has-checked:opacity-100"
+                                  />
+                                  <path
+                                    d="M3 7H11"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="opacity-0 group-has-indeterminate:opacity-100"
+                                  />
+                                </svg>
+                              </div>
+                            </div>
+                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
+                              {option.label}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </DisclosurePanel>
+                  </Disclosure>
+                ))}
+              </form>
+              </div>
+
             </DialogPanel>
           </div>
         </Dialog>
@@ -185,29 +238,25 @@ export default function Product() {
             </h2>
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
-              {/* Filters */}
-              <form className="hidden lg:block">
 
+              {/* Desktop Filters */}
+              <div className='hidden lg:block lg:col-span-1'>
+              <form className="mt-4 border-t border-gray-200">
                 {filters.map((section) => (
-                  <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
-                    <h3 className="-my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
+                    <h3 className="-mx-2 -my-3 flow-root">
+                      <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
-                        {/* <span className="ml-6 flex items-center">
-                          <PlusIcon aria-hidden="true" className="size-5 group-data-open:hidden" />
-                          <MinusIcon aria-hidden="true" className="size-5 group-not-data-open:hidden" />
-                        </span> */}
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
-                      <div className="space-y-4">
+                      <div className="space-y-6">
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex gap-3">
                             <div className="flex h-5 shrink-0 items-center">
                               <div className="group grid size-4 grid-cols-1">
                                 <input
                                   defaultValue={option.value}
-                                  defaultChecked={option.checked}
                                   id={`filter-${section.id}-${optionIdx}`}
                                   name={`${section.id}[]`}
                                   type="checkbox"
@@ -235,7 +284,10 @@ export default function Product() {
                                 </svg>
                               </div>
                             </div>
-                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
+                            <label
+                              htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
+                              className="min-w-0 flex-1 text-gray-500"
+                            >
                               {option.label}
                             </label>
                           </div>
@@ -245,6 +297,61 @@ export default function Product() {
                   </Disclosure>
                 ))}
               </form>
+                <form className="mt-4 border-t border-gray-200">
+                {singleFilter.map((section) => (
+                  <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
+                    <h3 className=" -mx-2 -my-3 flow-root">
+                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                        <span className="font-medium text-gray-900">{section.name}</span>
+                       
+                      </DisclosureButton>
+                    </h3>
+                    <DisclosurePanel className="pt-6">
+                      <div className="space-y-6">
+                        {section.options.map((option, optionIdx) => (
+                          <div key={option.value} className="flex gap-3">
+                            <div className="flex h-5 shrink-0 items-center">
+                              <div className="group grid size-4 grid-cols-1">
+                                <input
+                                  defaultValue={option.value}
+                                  id={`filter-${section.id}-${optionIdx}`}
+                                  name={section.id}
+                                  type="radio"
+                                  className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                                />
+                                <svg
+                                  fill="none"
+                                  viewBox="0 0 14 14"
+                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                                >
+                                  <path
+                                    d="M3 8L6 11L11 3.5"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="opacity-0 group-has-checked:opacity-100"
+                                  />
+                                  <path
+                                    d="M3 7H11"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="opacity-0 group-has-indeterminate:opacity-100"
+                                  />
+                                </svg>
+                              </div>
+                            </div>
+                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm cursor-pointer text-gray-600">
+                              {option.label}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </DisclosurePanel>
+                  </Disclosure>
+                ))}
+              </form>
+              </div>
 
               {/* Product grid */}
               <div className="lg:col-span-4 w-full">
@@ -253,6 +360,7 @@ export default function Product() {
                     {mens_kurta.map((item)=> <ProductCard product={item}/>)}
                 </div>
               </div>
+
             </div>
           </section>
         </main>
