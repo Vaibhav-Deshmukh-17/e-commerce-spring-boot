96d85e3845b0a5a8ec2eed83793a9afa2036606f
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-27 00:03:00 +0530

    wip: Product page

--- src/customer/component/Product/Product.jsx
+++ src/customer/component/Product/Product.jsx
@@ -71,7 +71,7 @@ export default function Product() {
                     {({open}) =>(
                       <>
                     <h3 className="-mx-2 -my-3 flow-root">
-                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
                           {open ? (
                               <MinusIcon className="h-5 w-5" aria-hidden="true" />
@@ -88,7 +88,7 @@ export default function Product() {
                               <div className="group grid size-4 grid-cols-1">
                                 <input
                                   defaultValue={option.value}
-                                  id={`filter-${section.id}-${optionIdx}`}
+                                  id={`filter-mobile-${section.id}-${optionIdx}`}
                                   name={`${section.id}[]`}
                                   type="checkbox"
                                   className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
@@ -135,8 +135,8 @@ export default function Product() {
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
                     {({open}) =>(
                       <>
-                    <h3 className=" -mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                    <h3 className="-mx-2 -my-3 flow-root">
+                      <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
                           {open ? (
                               <MinusIcon className="h-5 w-5" aria-hidden="true" />
@@ -146,14 +146,14 @@ export default function Product() {
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
-                      <div className="space-y-6">
+                      <div className="space-y-4">
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex gap-3">
                             <div className="flex h-5 shrink-0 items-center">
                               <div className="group grid size-4 grid-cols-1">
                                 <input
                                   defaultValue={option.value}
-                                  id={`filter-${section.id}-${optionIdx}`}
+                                  id={`filter-mobile-${section.id}-${optionIdx}`}
                                   name={section.id}
                                   type="radio"
                                   className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
@@ -180,7 +180,7 @@ export default function Product() {
                                 </svg>
                               </div>
                             </div>
-                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm cursor-pointer text-gray-600">
+                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
                               {option.label}
                             </label>
                           </div>
@@ -264,7 +264,7 @@ export default function Product() {
                     {({open}) =>(
                       <>
                     <h3 className="-mx-2 -my-3 flow-root">
-                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
                           {open ? (
                               <MinusIcon className="h-5 w-5" aria-hidden="true" />
@@ -309,7 +309,7 @@ export default function Product() {
                               </div>
                             </div>
                             <label
-                              htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
+                              htmlFor={`filter-${section.id}-${optionIdx}`}
                               className="min-w-0 flex-1 text-gray-500"
                             >
                               {option.label}
@@ -329,7 +329,7 @@ export default function Product() {
                     {({open}) =>(
                       <>
                     <h3 className=" -mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
                           {open ? (
                               <MinusIcon className="h-5 w-5" aria-hidden="true" />
