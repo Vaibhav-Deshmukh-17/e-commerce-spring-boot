00e4265d74e8e7944f01eabe7a3510adefcb74ae
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-26 23:50:31 +0530

    wip: product page

--- src/customer/component/Product/Product.jsx
+++ src/customer/component/Product/Product.jsx
@@ -65,12 +65,19 @@ export default function Product() {
 
               {/* Mobile Filters */}
               <div className='flex flex-col'>
-              <form className="mt-4 border-t border-gray-200">
+             <form className="mt-4 border-t border-gray-200">
                 {filters.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
+                    {({open}) =>(
+                      <>
                     <h3 className="-mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
+                          {open ? (
+                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                            ) : (
+                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                            )}
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
@@ -81,7 +88,7 @@ export default function Product() {
                               <div className="group grid size-4 grid-cols-1">
                                 <input
                                   defaultValue={option.value}
-                                  id={`filter-mobile-${section.id}-${optionIdx}`}
+                                  id={`filter-${section.id}-${optionIdx}`}
                                   name={`${section.id}[]`}
                                   type="checkbox"
                                   className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
@@ -118,27 +125,35 @@ export default function Product() {
                         ))}
                       </div>
                     </DisclosurePanel>
+                    </>
+                    )}
                   </Disclosure>
                 ))}
               </form>
                 <form className="mt-4 border-t border-gray-200">
                 {singleFilter.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
-                    <h3 className="-mx-2 -my-3 flow-root">
+                    {({open}) =>(
+                      <>
+                    <h3 className=" -mx-2 -my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
-                       
+                          {open ? (
+                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                            ) : (
+                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                            )}
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
-                      <div className="space-y-4">
+                      <div className="space-y-6">
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex gap-3">
                             <div className="flex h-5 shrink-0 items-center">
                               <div className="group grid size-4 grid-cols-1">
-                               <input
+                                <input
                                   defaultValue={option.value}
-                                  id={`filter-mobile-${section.id}-${optionIdx}`}
+                                  id={`filter-${section.id}-${optionIdx}`}
                                   name={section.id}
                                   type="radio"
                                   className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
@@ -165,13 +180,15 @@ export default function Product() {
                                 </svg>
                               </div>
                             </div>
-                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
+                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm cursor-pointer text-gray-600">
                               {option.label}
                             </label>
                           </div>
                         ))}
                       </div>
                     </DisclosurePanel>
+                    </>
+                    )}
                   </Disclosure>
                 ))}
               </form>
@@ -244,9 +261,16 @@ export default function Product() {
               <form className="mt-4 border-t border-gray-200">
                 {filters.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
+                    {({open}) =>(
+                      <>
                     <h3 className="-mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
+                          {open ? (
+                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                            ) : (
+                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                            )}
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
@@ -294,16 +318,24 @@ export default function Product() {
                         ))}
                       </div>
                     </DisclosurePanel>
+                    </>
+                    )}
                   </Disclosure>
                 ))}
               </form>
                 <form className="mt-4 border-t border-gray-200">
                 {singleFilter.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
+                    {({open}) =>(
+                      <>
                     <h3 className=" -mx-2 -my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
-                       
+                          {open ? (
+                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                            ) : (
+                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                            )}
                       </DisclosureButton>
                     </h3>
                     <DisclosurePanel className="pt-6">
@@ -348,6 +380,8 @@ export default function Product() {
                         ))}
                       </div>
                     </DisclosurePanel>
+                    </>
+                    )}
                   </Disclosure>
                 ))}
               </form>
