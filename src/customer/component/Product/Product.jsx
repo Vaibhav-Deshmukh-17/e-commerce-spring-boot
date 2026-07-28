391c8621103c0b003b60ff7261477abf5219e5b7
Vaibhav <deshmukhvaibhav697@gmail.com>
2026-07-28 22:14:11 +0530

    wip

--- src/customer/component/Product/Product.jsx
+++ src/customer/component/Product/Product.jsx
@@ -254,136 +254,139 @@ export default function Product() {
               Products
             </h2>
 
+                     {/* Desktop Filters */}
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
-              {/* Desktop Filters */}
-              <div className='hidden lg:block lg:col-span-1'>
-              <form className="mt-4 border-t border-gray-200">
-                {filters.map((section) => (
-                  <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
-                    {({open}) =>(
-                      <>
-                    <h3 className="-mx-2 -my-3 flow-root">
-                     <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">{section.name}</span>
-                          {open ? (
-                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                            ) : (
-                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                            )}
-                      </DisclosureButton>
-                    </h3>
-                    <DisclosurePanel className="pt-6">
-                      <div className="space-y-6">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex gap-3">
-                            <div className="flex h-5 shrink-0 items-center">
-                              <div className="group grid size-4 grid-cols-1">
-                                <input
-                                  defaultValue={option.value}
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={`${section.id}[]`}
-                                  type="checkbox"
-                                  className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                                />
-                                <svg
-                                  fill="none"
-                                  viewBox="0 0 14 14"
-                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+              <div>
+                <h1 className='text-lg font-semibold opacity-80 hidden lg:block'>Filter</h1>
+                <div className='hidden lg:block lg:col-span-1'>
+                  <form className="mt-4 border-t border-gray-200">
+                    {filters.map((section) => (
+                      <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
+                        {({open}) =>(
+                          <>
+                        <h3 className="-mx-2 -my-3 flow-root">
+                        <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
+                            <span className="font-medium text-gray-900">{section.name}</span>
+                              {open ? (
+                                  <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                                ) : (
+                                  <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                                )}
+                          </DisclosureButton>
+                        </h3>
+                        <DisclosurePanel className="pt-6">
+                          <div className="space-y-6">
+                            {section.options.map((option, optionIdx) => (
+                              <div key={option.value} className="flex gap-3">
+                                <div className="flex h-5 shrink-0 items-center">
+                                  <div className="group grid size-4 grid-cols-1">
+                                    <input
+                                      defaultValue={option.value}
+                                      id={`filter-${section.id}-${optionIdx}`}
+                                      name={`${section.id}[]`}
+                                      type="checkbox"
+                                      className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                                    />
+                                    <svg
+                                      fill="none"
+                                      viewBox="0 0 14 14"
+                                      className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                                    >
+                                      <path
+                                        d="M3 8L6 11L11 3.5"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="opacity-0 group-has-checked:opacity-100"
+                                      />
+                                      <path
+                                        d="M3 7H11"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="opacity-0 group-has-indeterminate:opacity-100"
+                                      />
+                                    </svg>
+                                  </div>
+                                </div>
+                                <label
+                                  htmlFor={`filter-${section.id}-${optionIdx}`}
+                                  className="min-w-0 flex-1 text-gray-500"
                                 >
-                                  <path
-                                    d="M3 8L6 11L11 3.5"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-checked:opacity-100"
-                                  />
-                                  <path
-                                    d="M3 7H11"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-indeterminate:opacity-100"
-                                  />
-                                </svg>
+                                  {option.label}
+                                </label>
                               </div>
-                            </div>
-                            <label
-                              htmlFor={`filter-${section.id}-${optionIdx}`}
-                              className="min-w-0 flex-1 text-gray-500"
-                            >
-                              {option.label}
-                            </label>
+                            ))}
                           </div>
-                        ))}
-                      </div>
-                    </DisclosurePanel>
-                    </>
-                    )}
-                  </Disclosure>
-                ))}
-              </form>
-                <form className="mt-4 border-t border-gray-200">
-                {singleFilter.map((section) => (
-                  <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
-                    {({open}) =>(
-                      <>
-                    <h3 className=" -mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">{section.name}</span>
-                          {open ? (
-                              <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                            ) : (
-                              <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                            )}
-                      </DisclosureButton>
-                    </h3>
-                    <DisclosurePanel className="pt-6">
-                      <div className="space-y-6">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex gap-3">
-                            <div className="flex h-5 shrink-0 items-center">
-                              <div className="group grid size-4 grid-cols-1">
-                                <input
-                                  defaultValue={option.value}
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={section.id}
-                                  type="radio"
-                                  className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                                />
-                                <svg
-                                  fill="none"
-                                  viewBox="0 0 14 14"
-                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
-                                >
-                                  <path
-                                    d="M3 8L6 11L11 3.5"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-checked:opacity-100"
-                                  />
-                                  <path
-                                    d="M3 7H11"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-indeterminate:opacity-100"
-                                  />
-                                </svg>
+                        </DisclosurePanel>
+                        </>
+                        )}
+                      </Disclosure>
+                    ))}
+                  </form>
+                    <form className="mt-4 border-t border-gray-200">
+                    {singleFilter.map((section) => (
+                      <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
+                        {({open}) =>(
+                          <>
+                        <h3 className=" -mx-2 -my-3 flow-root">
+                          <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 px-2 text-sm text-gray-400 hover:text-gray-500">
+                            <span className="font-medium text-gray-900">{section.name}</span>
+                              {open ? (
+                                  <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                                ) : (
+                                  <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                                )}
+                          </DisclosureButton>
+                        </h3>
+                        <DisclosurePanel className="pt-6">
+                          <div className="space-y-6">
+                            {section.options.map((option, optionIdx) => (
+                              <div key={option.value} className="flex gap-3">
+                                <div className="flex h-5 shrink-0 items-center">
+                                  <div className="group grid size-4 grid-cols-1">
+                                    <input
+                                      defaultValue={option.value}
+                                      id={`filter-${section.id}-${optionIdx}`}
+                                      name={section.id}
+                                      type="radio"
+                                      className="col-start-1 row-start-1 rounded-full border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                                    />
+                                    <svg
+                                      fill="none"
+                                      viewBox="0 0 14 14"
+                                      className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                                    >
+                                      <path
+                                        d="M3 8L6 11L11 3.5"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="opacity-0 group-has-checked:opacity-100"
+                                      />
+                                      <path
+                                        d="M3 7H11"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="opacity-0 group-has-indeterminate:opacity-100"
+                                      />
+                                    </svg>
+                                  </div>
+                                </div>
+                                <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm cursor-pointer text-gray-600">
+                                  {option.label}
+                                </label>
                               </div>
-                            </div>
-                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm cursor-pointer text-gray-600">
-                              {option.label}
-                            </label>
+                            ))}
                           </div>
-                        ))}
-                      </div>
-                    </DisclosurePanel>
-                    </>
-                    )}
-                  </Disclosure>
-                ))}
-              </form>
+                        </DisclosurePanel>
+                        </>
+                        )}
+                      </Disclosure>
+                    ))}
+                  </form>
+                </div>
               </div>
 
               {/* Product grid */}
